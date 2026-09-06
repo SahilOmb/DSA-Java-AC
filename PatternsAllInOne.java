5fa016d26b26d435d7e1cd737a4a585b173f0333
SyahilOmb <sahilomble22@gmail.com>
2026-09-06 18:01:12 +0530

    combined all pattern programs into one

---
+++ PatternsAllInOne.java
@@ -0,0 +1,66 @@
+
+import java.util.Scanner;
+
+public class PatternsAllInOne {
+
+     public static void StarPattern(){
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }   System.out.println();
+        }
+     }
+     
+     public static void InverseStarPattern(){
+         for (int i = 4; i >= 1; i--){
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }   System.out.println();
+         }
+     }
+
+    public static void CharacterPattern(){
+        char ch= 'A';
+         for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
+            }   System.out.println(); 
+                 
+    }     
+    }
+ 
+    public static void NumericalPattern(){
+        int n = 1 ;
+         for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(n);
+                n++;
+            }   System.out.println(); 
+                 
+    }     
+    }
+
+    public static void main (String args[]){
+            System.out.println("Chooese one:");
+            Scanner sc =new Scanner(System.in);
+            int Choice = sc.nextInt();
+           
+            switch (Choice) {
+                case 1:
+                    StarPattern();
+                    break;
+                case 2:
+                    InverseStarPattern();
+                    break;
+                case 3:
+                    CharacterPattern();
+                    break;
+                case 4:
+                    NumericalPattern();
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Data-Structures-and-Algos-Java---Apna-College
 🚀 A comprehensive tracker and repository for Data Structures and Algorithms (DSA) implemented from scratch in Java. Features clean, optimized code, time/space complexity analyses, and solutions to popular coding interview problems. ☕
+
+
+
